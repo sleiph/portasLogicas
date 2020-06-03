a69920339d4c4e0de108b431240f0ebb7880059e
Ricardo Gomes <sleiph@gmail.com>
2020-06-03 13:57:29 -0300

    novos scripts de controle das conexões

---
+++ scripts/linhaColisao.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linhaColisao : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "entrada")
+        {
+            cores.entrada = true;
+            Vector3 p = new Vector3 (col.transform.position.x-transform.position.x, col.transform.position.y-transform.position.y, 0);
+            cores.linePoints[1] = p;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "entrada")
+        {
+            cores.entrada = false;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ scripts/plugEntrada.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plugEntrada : MonoBehaviour
+{
+    SpriteRenderer thisCor;
+    void OnMouseOver()
+    {
+        thisCor.color = new Color(0.82f, 0.23f, 0.2f, 1f);
+    }
+    void OnMouseExit()
+    {
+        thisCor.color = new Color(0f, 0f, 0f, 1f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCor = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
