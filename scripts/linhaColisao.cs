461d20adaeb21f21d2aa56876e7b0e0827e8e314
Ricardo Gomes <sleiph@gmail.com>
2020-06-07 16:13:48 -0300

    o basico do programa funcionando

--- scripts/linhaColisao.cs
+++ scripts/linhaColisao.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class linhaColisao : MonoBehaviour
 {
-    public bool valorConectorEntrada = false;
+    public bool valorConector = false;
     private bool isOverAlguem = false;
     private bool isOverConector = false;
     private bool isMousedownConector = false;
-    private Vector3[] linhaEntrada = new Vector3[2];
+    private Vector3[] linhaCoords = new Vector3[2];
     private Vector3[] coliderCoords = new Vector3[2];
 
     private LineRenderer lr;
-    /// private CircleCollider2D colisor;
+    public linhaColisao irmao;
+    public GameObject conexao;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "entrada" || col.gameObject.tag == "saida"){
             isOverAlguem = true;
             coliderCoords[0] = new Vector3 (col.transform.position.x, col.transform.position.y,0);
         }
-        if (col.gameObject.tag == "saida"){
-            valorConectorEntrada = col.gameObject.GetComponent<plugSaida>().valor;
-        }
+        conexao = col.gameObject;
     }
     private void OnTriggerExit2D(Collider2D col)
     {
+        if (col.gameObject.tag == "saida")
+        {
+            valorConector = false;
+            irmao.valorConector = false;
+        }
         isOverAlguem = false;
-        valorConectorEntrada = false;
     }
     void OnMouseEnter()
     {
@@ -39,18 +42,52 @@ public class linhaColisao : MonoBehaviour
     void OnMouseDown()
     {
         isMousedownConector = true;
-        /// colisor.radius = .1f;
+        if (isOverAlguem){
+            if (conexao.tag == "entrada"){
+                conexao.GetComponent<plugSaida>().valor = false;
+            }
+            else if (conexao.tag == "saida"){
+                valorConector = false;
+                irmao.valorConector = false;
+                if (irmao.isOverAlguem && irmao.conexao.tag == "entrada"){
+                    irmao.AssignValor(valorConector);
+                }
+            }
+        }
     }
     void OnMouseUp()
     {
         isMousedownConector = false;
-        /// colisor.radius = .33f;
         transform.position = new Vector3(transform.position.x, transform.position.y, -3);
+        if (isOverAlguem){
+            if (conexao.tag == "entrada"){
+                AssignValor(valorConector);
+                Debug.Log("entradaMouseup " + valorConector);
+            }
+            else if (conexao.tag == "saida"){
+                valorConector = conexao.GetComponent<plugSaida>().valor;
+                irmao.valorConector = conexao.GetComponent<plugSaida>().valor;
+                if (irmao.isOverAlguem && irmao.conexao.tag == "entrada"){
+                    irmao.AssignValor(valorConector);
+                }
+            }
+        }
     }
+
+    public void AssignValor(bool valor)
+    {
+        conexao.GetComponent<plugSaida>().valor = valorConector;
+    }
+
     void Awake()
     {
         lr = gameObject.GetComponentInParent<LineRenderer>();
-        /// colisor = GetComponent<CircleCollider2D>();
+        if (transform.gameObject.tag == "comeco"){
+            irmao = transform.parent.gameObject.transform.GetChild(1).gameObject.GetComponent<linhaColisao>();
+        }
+        else{
+            irmao = transform.parent.gameObject.transform.GetChild(0).gameObject.GetComponent<linhaColisao>();
+        }
     }
     // Start is called before the first frame update
     void Start()
@@ -62,17 +99,28 @@ public class linhaColisao : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButton (0) && isMousedownConector) {
-            if (!plugEntrada.isOverEntrada && !plugSaida.isOverMan){
+            if (!plugSaida.isOverMan){
                 Camera c = Camera.main;
                 Vector3 p = c.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 10));
-                linhaEntrada[0] = p;
-                lr.SetPosition (0, linhaEntrada[0]);
-                this.transform.position = new Vector3 (linhaEntrada[0][0], linhaEntrada[0][1], linhaEntrada[0][2]);
+                linhaCoords[0] = p;
+                if (transform.gameObject.tag == "comeco"){
+                    lr.SetPosition (0, linhaCoords[0]);
+                }
+                else{
+                    lr.SetPosition (1, linhaCoords[0]);
+                }
+                this.transform.position = new Vector3 (linhaCoords[0][0], linhaCoords[0][1], linhaCoords[0][2]);
             }
             else{
-                lr.SetPosition (0, coliderCoords[0]);
+                if (transform.gameObject.tag == "comeco"){
+                    lr.SetPosition (0, coliderCoords[0]);
+                }
+                else{
+                    lr.SetPosition (1, coliderCoords[0]);
+                }
                 this.transform.position = new Vector3 (coliderCoords[0][0], coliderCoords[0][1], coliderCoords[0][2]);
             }
         }
+        /// if (valorConector != irmao.valorConector) valorConector = valorConector | irmao.valorConector;
     }
 }
--- scripts/plugSaida.cs
+++ scripts/plugSaida.cs
@@ -10,24 +10,33 @@ public class plugSaida : MonoBehaviour
     public static bool isOverMan = false;
 
     SpriteRenderer thisCor;
+    public plugSaida entrada2;
+    public plugSaida entrada4;
 
     public void TrueCheck (bool bule)
     {
         foreach (Transform child in transform.parent)     
-        {  
-            if (child.tag == "cor") child.gameObject.SetActive(bule);
-            else if (child.tag == "notCor") child.gameObject.SetActive(!bule);
+        {
+            if (transform.gameObject.tag == "saida") if (child.tag == "cor") child.gameObject.SetActive(bule);
+            if (transform.parent.gameObject.tag == "NAND" || transform.parent.gameObject.tag == "NOR" || transform.parent.gameObject.tag == "XNOR"){
+                if (child.tag == "notCor") child.gameObject.SetActive(!bule);
+            }
+            if (child.name == "plugEntrada2") entrada2 = child.gameObject.GetComponent<plugSaida>();
+            else if (child.name == "plugEntrada4") entrada4 = child.gameObject.GetComponent<plugSaida>();
             v_valor = bule;
-        } 
+        }
     }
     void OnMouseEnter()
     {
         thisCor.color = new Color(0.82f, 0.23f, 0.2f, 1f);
         isOverMan = true;
+        if (transform.gameObject.tag == "entrada"){
+            if (valor != v_valor) TrueCheck(valor);
+        }
     }
     void OnMouseExit()
     {
-        thisCor.color = new Color(0f, 0f, 0f, 1f);
+        thisCor.color = new Color(1f, 1f, 1f, 1f);
         isOverMan = false;
     }
     void OnMouseDown()
@@ -42,12 +51,39 @@ public class plugSaida : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+    void Awake()
+    {
         thisCor = GetComponent<SpriteRenderer>();
+        TrueCheck(valor);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (valor != v_valor){TrueCheck(valor);}
+        if (transform.gameObject.tag == "saida"){
+            if (valor != v_valor) TrueCheck(valor);
+        }
+        if (transform.gameObject.tag == "saida" && transform.parent.gameObject.tag != "fonte"){
+            if (transform.parent.gameObject.tag == "AND"){
+                valor = entrada2.valor & entrada4.valor;
+            }
+            else if (transform.parent.gameObject.tag == "OR"){
+                valor = entrada2.valor | entrada4.valor;
+            }
+            else if (transform.parent.gameObject.tag == "NAND"){
+                valor = !entrada2.valor & !entrada4.valor;
+            }
+            else if (transform.parent.gameObject.tag == "NOR"){
+                valor = !entrada2.valor | !entrada4.valor;
+            }
+            else if (transform.parent.gameObject.tag == "XOR"){
+                valor = entrada2.valor != entrada4.valor;
+            }
+            else if (transform.parent.gameObject.tag == "XNOR"){
+                valor = !(entrada2.valor != entrada4.valor);
+            }
+        }
     }
 }
